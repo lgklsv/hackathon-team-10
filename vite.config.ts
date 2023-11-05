daa164b5780438ecba3b8da249002aa0e037e6fc
lgklsv <kolosov20011@gmail.com>
2023-11-05 16:18:11 +0400

    Add configs, linters, formatters and basic vite boilerplate

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '',
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+  }
+})
