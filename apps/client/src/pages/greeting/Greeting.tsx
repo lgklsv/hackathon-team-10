d0c997514b0eebc3ea47dffaf4804ef2c99e44e0
Marr <marat.salah1313@gmail.com>
2023-11-11 18:19:21 +0100

    add Greeting

---
+++ apps/client/src/pages/greeting/Greeting.tsx
@@ -0,0 +1,20 @@
+function Greeting() {
+  return (
+    <>
+      <header>
+        <h1>Приветствую!</h1>
+      </header>
+      <div>
+        <span>Выберите уровень сложности</span>
+        <button type="button">Высокий</button>
+        <button type="button">Средний</button>
+        <button type="button">Низкий</button>
+      </div>
+      <div>
+        <button type="button">Начать игру</button>
+      </div>
+    </>
+  )
+}
+
+export default Greeting
