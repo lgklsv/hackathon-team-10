7a31d2a9978f5af9bf8864a35d271dd0956b1b62
lgklsv <kolosov20011@gmail.com>
2023-11-12 16:39:07 +0400

    Fix mobile menu

--- apps/client/src/widgets/adaptive_menu/AdaptiveMenu.tsx
+++ apps/client/src/widgets/adaptive_menu/AdaptiveMenu.tsx
@@ -1,30 +1,28 @@
-import React from 'react';
-import Media  from 'react-media';
-import MobileMenu from "@/widgets/mobile_menu/MobileMenu.tsx";
-import {Header} from "@/widgets/header";
+import React from 'react'
 
-interface MenuProps {
-  // Передавайте пропсы для мобильного и обычного меню
-}
+import Media from 'react-media'
 
+import { Header } from '@/widgets/header'
+import MobileMenu from '@/widgets/mobile_menu/MobileMenu.tsx'
 
-const AdaptiveMenu = () => {
+function AdaptiveMenu() {
   return (
-    <Media queries={
-      { small: '(min-width: 320px) and (max-width: 767px)',
-        medium: "(min-width: 768px) and (max-width: 1199px)",
-        large: "(min-width: 1200px)"}
-    }>
+    <Media
+      queries={{
+        small: '(min-width: 320px) and (max-width: 767px)',
+        medium: '(min-width: 768px) and (max-width: 1199px)',
+        large: '(min-width: 1200px)'
+      }}
+    >
       {(matches: any) => (
         <>
-          {matches.small && <MobileMenu/>}
-          {matches.medium && <Header/>}
-          {matches.large && <Header/>}
+          {matches.small && <MobileMenu />}
+          {matches.medium && <Header />}
+          {matches.large && <Header />}
         </>
       )}
     </Media>
-  );
+  )
 }
 
-export default AdaptiveMenu;
-
+export default AdaptiveMenu
--- apps/client/src/widgets/mobile_menu/MobileMenu.tsx
+++ apps/client/src/widgets/mobile_menu/MobileMenu.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import styles from "@/widgets/mobile_menu/mobile_menu.module.css";
-import {Button} from "@/shared/ui";
+import React from 'react'
+
+import Select, { SingleValue } from 'react-select'
+
 import {
   MazeDifficulty,
   MazeStatus,
@@ -8,26 +9,32 @@ import {
   restartLevel,
   selectIsSolutionMode,
   selectMazeStatus,
-  setGameRestarting, setMazeDifficulty, toggleSolutionMode
-} from "@/entities/maze";
-import Select, {SingleValue} from "react-select";
-import {useAppDispatch, useAppSelector} from "@/shared/hooks";
-import {selectIsMenuOpen, setModal} from "@/entities/menu";
+  setGameRestarting,
+  setMazeDifficulty,
+  toggleSolutionMode
+} from '@/entities/maze'
+import { selectIsMenuOpen, setModal } from '@/entities/menu'
+import { useAppDispatch, useAppSelector } from '@/shared/hooks'
+import { Button } from '@/shared/ui'
 
+import styles from './MobileMenu.module.css'
 
 const options = [
   { value: MazeDifficulty.easy, label: 'Легко' },
   { value: MazeDifficulty.medium, label: 'Средне' },
   { value: MazeDifficulty.hard, label: 'Тяжело' }
 ]
-const MobileMenu = () => {
+
+function MobileMenu() {
   const dispatch = useAppDispatch()
   const isOpen = useAppSelector(selectIsMenuOpen)
   const solutionMode = useAppSelector(selectIsSolutionMode)
   const status = useAppSelector(selectMazeStatus)
+  const [openMenu, setOpenMenu] = React.useState(false)
 
   const openModalHandler = () => {
     dispatch(setModal(!isOpen))
+    setOpenMenu(false)
   }
 
   const restartGameHandler = () => {
@@ -40,6 +47,7 @@ const MobileMenu = () => {
 
   const restartLevelHandler = () => {
     dispatch(restartLevel())
+    setOpenMenu(false)
   }
 
   const toggleMazeSolutionHandler = () => {
@@ -52,52 +60,52 @@ const MobileMenu = () => {
     if (e?.value) dispatch(setMazeDifficulty(e.value))
   }
 
-  const [openMenu, setOpenMenu] = React.useState(false)
-
-
   return (
     <>
       <div className={styles.open_menu}>
         <Button
-          onClick={()=>{setOpenMenu(!openMenu)}}>
-          <h3>{!openMenu ? 'Открыть меню' : 'Закрыть меню'}</h3>
-        </Button>
-      </div>
-      <header className={openMenu ? styles.header_open : styles.header_close}>
-      <nav className={styles.header__content}>
-        <Button
           onClick={restartGameHandler}
           variant={status === MazeStatus.playing ? 'default' : 'secondary'}
         >
-          {status === MazeStatus.playing ? 'Завершить' : 'Новая игра'}{' '}
+          {status === MazeStatus.playing ? 'Завершить' : 'Новая игра'}
         </Button>
-        <Button
-          onClick={restartLevelHandler}
-          disabled={status !== MazeStatus.playing}
-        >
-          Сброс уровня
-        </Button>
-        <Button
-          className={styles.header__hint}
-          onClick={toggleMazeSolutionHandler}
-          disabled={status !== MazeStatus.playing}
-        >
-          {solutionMode && status === MazeStatus.playing
-            ? 'Выключить подсказку'
-            : 'Включить подсказку'}
-        </Button>
-        <Button onClick={openModalHandler}>Как играть?</Button>
         <Select
-          className={styles.header__select}
+          className={styles.select}
           defaultValue={options[0]}
           options={options}
           onChange={changeDifficultyHandler}
           placeholder="Выберите сложность"
         />
-      </nav>
-    </header>
+        <Button
+          onClick={() => {
+            setOpenMenu(!openMenu)
+          }}
+        >
+          {!openMenu ? 'Открыть меню' : 'Закрыть меню'}
+        </Button>
+      </div>
+      <header className={openMenu ? styles.header_open : styles.header_close}>
+        <nav className={styles.header__content}>
+          <Button
+            onClick={restartLevelHandler}
+            disabled={status !== MazeStatus.playing}
+          >
+            Сброс уровня
+          </Button>
+          <Button
+            className={styles.header__hint}
+            onClick={toggleMazeSolutionHandler}
+            disabled={status !== MazeStatus.playing}
+          >
+            {solutionMode && status === MazeStatus.playing
+              ? 'Выключить подсказку'
+              : 'Включить подсказку'}
+          </Button>
+          <Button onClick={openModalHandler}>Как играть?</Button>
+        </nav>
+      </header>
     </>
   )
-};
+}
 
-export default MobileMenu;
+export default MobileMenu
