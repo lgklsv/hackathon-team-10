781fe106d68d528e128b7f563fb51ca6e8feafad
lgklsv <kolosov20011@gmail.com>
2023-11-12 15:18:25 +0400

    Improve instrucations a bit

--- apps/client/src/entities/instruction/ui/InstructionModalContent.tsx
+++ apps/client/src/entities/instruction/ui/InstructionModalContent.tsx
@@ -1,4 +1,5 @@
 import { MoveDown, MoveLeft, MoveRight, MoveUp } from 'lucide-react'
+
 import styles from './instruction.module.css'
 
 export default function InstructionModalContent() {
@@ -7,18 +8,25 @@ export default function InstructionModalContent() {
       <h1>Здравствуй, дорогой Игрок!</h1>
       <h2>Как играть?</h2>
       <div className={styles.list}>
-        <p>1. Вы играете за родителя в левой верхней части лабиринта</p>
-        <p>
-          2. Для победы Вам нужно пройти в правую нижнюю часть лабиринта и
-          встретиться с ребенком
-        </p>
-        <p>
-          3. Для передвижения используйте клавиши <MoveUp />
-          <MoveDown />
-          <MoveLeft />
-          <MoveRight />
-        </p>
-        <p>4. Если Вы хотите поменять лабиринт нажмите кнопку Reset</p>
+        <ol>
+          <li> Вы играете за родителя в левой верхней части лабиринта</li>
+          <li>
+            Для победы Вам нужно пройти в правую нижнюю часть лабиринта и
+            встретиться с ребенком
+          </li>
+          <li>
+            <div className={styles.controls}>
+              Для передвижения используйте клавиши <MoveUp />
+              <MoveDown />
+              <MoveLeft />
+              <MoveRight />
+            </div>
+          </li>
+          <li>
+            Если Вы хотите поменять лабиринт нажмите кнопку завершить и затем
+            новая игра
+          </li>
+        </ol>
       </div>
       <h2>Приятной игры!</h2>
     </div>
